d0f2ff413903afb89a5b76460d6def9f7d425d9a
Aatish Dhami <aatishdhami@Aatishs-MBP.home>
2022-10-04 02:22:55 -0700

    finished SoundexTokenProcessor

--- indexing/soundexindex.py
+++ indexing/soundexindex.py
@@ -10,13 +10,13 @@ class SoundexIndex(Index):
         """Constructs an empty SoundingIndex using dictionary."""
         self.vocab = {}
 
-    def add_term(self, hash: str):
+    def add_term(self, hashcode: str):
         """Records that the given hash occurred in the given document ID."""
         pass
 
-    def getPostings(self, hash: str) -> Iterable[Posting]:
+    def getPostings(self, hashcode: str) -> Iterable[Posting]:
         """Returns a list of Postings for all documents that contain the given hash."""
-        return self.vocab.get(hash, [])
+        return self.vocab.get(hashcode, [])
 
     def getVocabulary(self) -> Iterable[str]:
         return sorted(list(self.vocab.keys()))
--- text/soundextokenprocessor.py
+++ text/soundextokenprocessor.py
@@ -21,6 +21,39 @@ class SoundexTokenProcessor(TokenProcessor):
     5. Remove all zeros from the resulting string. Pad the resulting string with trailing zeros and return the first
         four positions, which will consist of a letter followed by three digits. """
 
+    def process_token(self, token: str) -> str:
+        token = token.upper()
+        fhashcode = token[0]
+        hashcode = ""
+        dictionary = {"AEIOUHWY": ".",
+                      "BFPV": "1",
+                      "CGJKQSXZ": "2",
+                      "DT": "3",
+                      "L": "4",
+                      "MN": "5",
+                      "R": "6"
+                      }
 
-    def process_token(self, token: str) -> list[str]:
-        pass
+        # Encoding
+        for char in token[1:]:
+            for key in dictionary.keys():
+                if char in key:
+                    code = dictionary[key]
+                    if code != '.':
+                        if code != fhashcode[-1]:
+                            fhashcode += code
+
+        i = 0
+        while i < len(fhashcode) - 1:
+            hashcode += fhashcode[i]
+            if fhashcode[i] != fhashcode[i + 1]:
+                i += 1
+            else:
+                i += 2
+        if i < len(fhashcode):
+            hashcode += fhashcode[-1]
+
+
+        hashcode = hashcode[:4].ljust(4, "0")
+
+        return hashcode
